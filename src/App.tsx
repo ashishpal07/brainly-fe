0115bef330a6b71a89df38c27f8c1b3b3dc0cc53
ashishpal07 <ashishpal7861@gmai.com>
2025-01-21 22:14:49 +0530

    added card

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import './App.css'
-import { Button } from './components/ui/Button'
-import { Card } from './components/ui/Card'
-import { PlusIcon } from './icons/PlusIcon'
-import { ShareIcon } from './icons/ShareIcon'
+import "./App.css";
+import { Button } from "./components/ui/Button";
+import { Card } from "./components/ui/Card";
+import { PlusIcon } from "./icons/PlusIcon";
+import { ShareIcon } from "./icons/ShareIcon";
 
 function App() {
   return (
     <>
-    <Button startIcon={<PlusIcon size={"md"} />} variant='primary' text='Share Brain' size='sm' />
-    <Button startIcon={<ShareIcon size={"md"} />} variant='secondary' text='Add Content' size='sm' />
-    <Card />
+      <Button
+        startIcon={<PlusIcon size={"md"} />}
+        variant="primary"
+        text="Share Brain"
+        size="sm"
+      />
+      <Button
+        startIcon={<ShareIcon size={"md"} />}
+        variant="secondary"
+        text="Add Content"
+        size="sm"
+      />
+      <Card
+        type={"youtube"}
+        link={"https://www.youtube.com/embed/PtPPWEgaWLg?si=m5zGfUjblD6ehRvD"}
+        title={"Youtube Video"}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
